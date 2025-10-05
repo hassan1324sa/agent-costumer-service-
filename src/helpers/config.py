599bafcdfde08b86d46da8756eb316957a5bc247
hassan1324as <hassan123smn@gmail.com>
2025-10-05 23:31:32 +0300

    modify booking

--- src/helpers/config.py
+++ src/helpers/config.py
@@ -4,6 +4,10 @@ class Settings(BaseSettings):
     APP_NAME: str
     MONGODB_URL: str
     MONGODB_DATABASE: str
+    COHERE_API_KEY:str
+    AGENT_SECRET_KEY:str
+    CHROMA_DB_PATH:str
+    FASTAPI_URL:str
 
     model_config = SettingsConfigDict(
         env_file=".env",
--- src/routers/booking.py
+++ src/routers/booking.py
@@ -1,16 +1,28 @@
-from fastapi import APIRouter, Request, Depends, status
+from fastapi import APIRouter, Request, Depends, Header, HTTPException, status
 from fastapi.responses import JSONResponse
-from controllers.bookingController import BookingController
 from helpers.config import getSettings, Settings
 from models import ResponseEnum, BookingModel
-from helpers.jsonEncoder import bson_to_json
+from controllers.bookingController import BookingController
+from bson import ObjectId
 
 bookingRouter = APIRouter(prefix="/api/booking")
+settings = getSettings()
 
+async def verify_agent(authorization: str = Header(...)):
+    if authorization != f"Bearer {settings.AGENT_SECRET_KEY}":
+        raise HTTPException(status_code=403, detail="Unauthorized agent")
 
 
 @bookingRouter.post("/add/")
-async def addBooking(request: Request, username: str, service_type: str, date: str, time: str, appSettings: Settings = Depends(getSettings)):
+async def addBooking(
+    request: Request,
+    username: str,
+    service_type: str,
+    date: str,
+    time: str,
+    appSettings: Settings = Depends(getSettings),
+    _: None = Depends(verify_agent),  
+):
     try:
         dbClient = request.app.db_client
         controller = BookingController()
@@ -31,13 +43,21 @@ async def addBooking(request: Request, username: str, service_type: str, date: s
 
 
 @bookingRouter.get("/all/")
-async def getAllBookings(request: Request, username: str, appSettings: Settings = Depends(getSettings)):
+async def getAllBookings(
+    request: Request,
+    username: str,
+    appSettings: Settings = Depends(getSettings),
+    _: None = Depends(verify_agent), 
+):
     dbClient = request.app.db_client
     bookingModel = await BookingModel.createInstance(dbClient=dbClient)
 
     try:
         user_bookings = await bookingModel.getBookings(username)
-        user_bookings = bson_to_json(user_bookings)
+
+        for booking in user_bookings:
+            booking["_id"] = str(booking["_id"])
+            booking["created_at"] = str(booking["created_at"])
 
         return JSONResponse(
             content={
